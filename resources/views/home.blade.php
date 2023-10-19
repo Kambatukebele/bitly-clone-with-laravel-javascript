26eb1ec15de45c5b20aba3235700438314b08515
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2023-10-19 12:20:46 +0200

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-    <h1 class="font-poppins text-lg text-red-400 ">Hello There</h1>
-</body>
-</html>
+<x-web-site-layout>
+    <h1>Here goest the content</h1>
+</x-web-site-layout>
+    
